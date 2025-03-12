d97636a3d95eb3bb0ff875b1650575c0ba034e8f
Kanhaiya960 <168913838+Kanhaiya960@users.noreply.github.com>
2025-03-12 10:12:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -141,20 +141,20 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', True))
+VERIFY = bool(environ.get('VERIFY', False)
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'techvjlink.site')
 VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '5d84c45bd97704c052ca8355670f0d024083f3fb')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/Movies_4_Download/20')
 
 # If You Fill Second Shortner Then Bot Attach Both First And Second Shortner And Use It For Verify.
-VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
 # if verify second shortner is True then fill below url and api
+VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
 VERIFY_SND_SHORTLINK_URL = environ.get('VERIFY_SND_SHORTLINK_URL', 'techvjlink.site')
 VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '5d84c45bd97704c052ca8355670f0d024083f3fb')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'techvjlink.site')
 SHORTLINK_API = environ.get('SHORTLINK_API', '5d84c45bd97704c052ca8355670f0d024083f3fb')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/Movies_4_Download/5') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
