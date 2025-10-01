762d397a706e173287f96d41451283af57b2cc10
Kanhaiya960 <168913838+Kanhaiya960@users.noreply.github.com>
2025-10-01 14:04:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -154,7 +154,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', Fals)) # Set True Or False
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'vplink.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '8585479c122feb4c124dbad84240f316e5b6de21')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/Movies_4_Download/633') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
@@ -219,3 +219,4 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
